d8014212061439a178b8a3446cfccd584f299378
Bram Gotink <bram@gotink.me>
2022-07-16 22:35:56 +0200

    {chore} add small benchmark script
    While the target of this package isn't to be the fastest
    per se, it's always nice to be able to inspect the impact
    of changes on performance.

---
+++ scripts/bench.js
@@ -0,0 +1,81 @@
+import bench from 'benchmark';
+import * as kdljs from 'kdljs';
+import {stderr} from 'node:process';
+
+import * as dev from '../src/index.js';
+
+let built;
+try {
+	built = await import('../out/index.mjs');
+} catch {
+	stderr.write(
+		'No out folder found, run `yarn build` to include the built version of @bgotink/kdl\n',
+	);
+	built = null;
+}
+
+const suite = new bench.Suite();
+
+const document = String.raw`
+node \
+{
+	child; "child"; r###"child"###;
+}
+
+deeply { nested { node { but { like { really { deeply { nested { or { whatever; }; }; }; }; }; }; }; }; }
+
+node \
+  with="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+  r#"no really,"#="a lot" \
+	"of" "children" 4.20;
+`;
+
+// sanity check: assert document parses
+kdljs.parse(document);
+dev.parse(document);
+built?.parse(document);
+
+suite.add('kdljs#parse', () => {
+	kdljs.parse(document);
+});
+
+suite.add('development #parse', () => {
+	dev.parse(document);
+});
+
+if (built) {
+	const _built = built;
+	suite.add('built #parse', () => {
+		_built.parse(document);
+	});
+}
+
+// add listeners
+suite
+	.on(
+		'cycle',
+		/** @param {bench.Event} event */ function (event) {
+			console.log(String(event.target));
+		},
+	)
+	.on(
+		'complete',
+		/** @this {bench.Suite} */ function () {
+			console.log('Fastest is ' + this.filter('fastest').map('name'));
+		},
+	)
+	.run();
