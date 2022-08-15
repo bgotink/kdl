daa775f02a9a75d65c6fd11e48734ac6ac4d747e
Bram Gotink <bram@gotink.me>
2022-08-15 13:33:32 +0200

    {fix} update test after rewording of error message

--- test/errors.js
+++ test/errors.js
@@ -5,13 +5,13 @@ import {parse} from '../src/index.js';
 
 test('unterminated node', () => {
 	expect(() => parse(`node {child}`)).toThrowError(
-		/missing `;` between child node and `}` at 1:12/,
+		/missing ";" between child node and "}" at 1:12/,
 	);
 	expect(() => parse(`node sibling "lorem"`)).toThrowError(
-		/missing `;` or newline between two sibling nodes, or missing `=` to define a property at 1:13/,
+		/missing ";" or newline between two sibling nodes, or missing "=" to define a property at 1:13/,
 	);
 	expect(() => parse(`node sibling`)).toThrowError(
-		/missing `;` or newline between two sibling nodes, or missing `=` to define a property at end of file/,
+		/missing ";" or newline between two sibling nodes, or missing "=" to define a property at end of input/,
 	);
 });
 
